0bf2566ffb315fd0151ed3ca6735504c23e27c31
mena5800 <mr.safwat55@gmail.com>
2023-06-07 02:07:51 +0300

    solev problem 13 in c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -7,5 +7,41 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    
+	listint_t *current, *next, *new;
+
+	current = *head;
+	new = malloc(sizeof(listint_t));
+	new->n = number;
+	new->next = NULL;
+	if (current == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	while (current)
+	{
+		if (current->n > number)
+		{
+			next = current;
+			new->next = current;
+			*head = new;
+			return (new);
+		}
+		else if (current->next != NULL)
+		{
+			if (current->next->n > number)
+			{
+				next = current->next;
+				current->next = new;
+				new->next = next;
+				return (new);
+			}
+			current = current->next;
+		}
+		else
+		{
+			current->next = new;
+			return (new);
+		}
+	}
 }
