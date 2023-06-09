a9897a5c7e2ecbe821249b71758f99467590ee78
mena5800 <mr.safwat55@gmail.com>
2023-06-09 04:52:57 +0300

    solve 1 - 2 - 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-print(f"{len(sys.argv) -1} arguments",end="")
+if __name__ == "__main__":
+    import sys
+    print(f"{len(sys.argv) -1} arguments", end="")
 
-if (len(sys.argv) - 1 == 0):
-    print(".")
-else:
-    print(":")
+    if (len(sys.argv) - 1 == 0):
+        print(".")
+    else:
+        print(":")
 
-for i in range(1,len(sys.argv)):
-    print(f"{i}: {sys.argv[i]}")
+    for i in range(1, len(sys.argv)):
+        print(f"{i}: {sys.argv[i]}")
