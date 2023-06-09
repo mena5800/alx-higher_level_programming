6e74d3a7abf5da462d3b7e6edc692bbb930f47ba
mena5800 <mr.safwat55@gmail.com>
2023-06-09 04:44:50 +0300

    fix bug

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 # 1/usr/bin/python3
-import add_0
-a = 1
-b = 2
-print(f"{a} + {b} = {add_0.add(a, b)}")
+if __name__ == "__main__":
+    """print the sum of 1 and 2."""
+    from add_0 import add
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
