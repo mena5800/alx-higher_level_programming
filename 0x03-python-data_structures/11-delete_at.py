f9d0e5c8dfa002a06eb71b5ca061c8a2510966d1
mena5800 <mr.safwat55@gmail.com>
2023-06-10 12:16:43 +0300

    solve 7 - 8 - 9 - 10

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    new_list = []
+    for i in range(len(my_list)):
+        if (i != idx):
+            new_list.append(my_list[i])
+    my_list = new_list
+    return my_list
+
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
+
+
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def clean_tuple(tup=()):
+    if len(tup) == 0:
+        return (0, 0)
+    elif len(tup) == 1:
+        return (tup[0], 0)
+    else:
+        return tup
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = clean_tuple(tuple_a)
+    tuple_b = clean_tuple(tuple_b)
+    tuple_c = (tuple_a[0]+tuple_b[0], tuple_a[1]+tuple_b[1])
+    return tuple_c
