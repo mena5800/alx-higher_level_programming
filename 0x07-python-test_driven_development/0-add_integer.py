cb60db8a845290b4b82a6f86770c958ebb743d19
mena5800 <mr.safwat55@gmail.com>
2023-07-03 01:37:41 +0300

    add test driven

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+def add_integer(a, b=98):
+    '''
+    this function takes two numbers as input and return their sum.
+
+    parameters:
+    a (int or float): the first number to add.
+    b (int or float): the second number to add.
+
+    Returns:
+    int: the sum of the two numbers.
+    '''
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+
+    return a + b
