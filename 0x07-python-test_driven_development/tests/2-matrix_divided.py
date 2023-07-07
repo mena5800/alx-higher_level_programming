bde4d032bd449b5d0646015aabe81a2ab5284d49
mena5800 <mr.safwat55@gmail.com>
2023-07-08 00:38:42 +0300

    add 2-

--- 0x07-python-test_driven_development/tests/2-matrix_divided.py
+++ 0x07-python-test_driven_development/tests/2-matrix_divided.py
@@ -26,9 +26,9 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
         for j in range(len(matrix[0])):
             if type(matrix[i][j]) not in [float, int]:
-                massege = "matrix must be a matrix (list of lists) "
-                massege += "of integers/floats"
-                raise TypeError(massege)
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats"
+                    )
 
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
