90ce5c7f631befc587be09b1a7e0ca3948c6013d
mena5800 <mr.safwat55@gmail.com>
2023-07-11 03:39:21 +0300

    add 10 - 11

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    this class contain only one function init
+    """
+
+    def __init__(self, size):
+        """
+        initialization function
+        """
+        self.__size = size
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+
+    def area(self):
+        """
+        fucntion return the area of square
+        """
+        return self.__size * self.__size
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    this class contain only one function init
+    """
+
+    def __init__(self, size):
+        """
+        initialization function
+        """
+        self.__size = size
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+
+    def area(self):
+        """
+        fucntion return the area of square
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        fucntion to return informations about instance
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
