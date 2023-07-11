18a289515c6bc4c49f45be845ce04a010902ac61
mena5800 <mr.safwat55@gmail.com>
2023-07-11 03:26:12 +0300

    solve 8 9

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,44 @@
 #!/usr/bin/python3
+"""
+this module class Rectangle
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+
+    def area(self):
+        """
+        fucntion to raise error
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        function to check that value is int and > 0
+        """
+        if type(value) is not int:
+            error_message = "{} must be an integer".format(name)
+            raise TypeError(error_message)
+
+        if value <= 0:
+            error_message = "{} must be greater than 0".format(name)
+            raise ValueError(error_message)
+
+
+class Rectangle(BaseGeometry):
+    """
+    class Rectangle inherit from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        """
+        initialization function
+        """
+        self.__width = width
+        self.__height = height
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,1 +1,53 @@
 #!/usr/bin/python3
+"""
+this module class Rectangle
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+
+    def area(self):
+        """
+        fucntion to raise error
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        function to check that value is int and > 0
+        """
+        if type(value) is not int:
+            error_message = "{} must be an integer".format(name)
+            raise TypeError(error_message)
+
+        if value <= 0:
+            error_message = "{} must be greater than 0".format(name)
+            raise ValueError(error_message)
+
+
+class Rectangle(BaseGeometry):
+    """
+    class Rectangle inherit from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        """
+        initialization function
+        """
+        self.__width = width
+        self.__height = height
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """
+        function to return the area of rectangle
+        """
+        return self.__height * self.__width
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
