c93246752b0496e0381e32d946290403622cd299
mena5800 <mr.safwat55@gmail.com>
2023-07-18 05:57:29 +0300

    add square comment

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,6 +8,9 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """
+    module square
+    """
     def __init__(self, size, x=0, y=0, id=None):
         """
         init function
