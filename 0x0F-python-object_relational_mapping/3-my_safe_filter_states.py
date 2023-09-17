a7bc707363f808ed0837973350ca35b3940f1187
mena5800 <mr.safwat55@gmail.com>
2023-09-17 22:08:12 +0300

    fix pycode style

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where
-name matches the argument. But this time, write one that is safe from MySQL injections!"""
+"""script that takes in arguments and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument. But this time,
+write one that is safe from MySQL injections!"""
 
 import MySQLdb
 from sys import argv
@@ -12,7 +13,7 @@ if __name__ == "__main__":
     search_term = argv[4]
     host = "localhost"
     port = 3306
-    if  '"' not in search_term and "'" not in search_term: 
+    if '"' not in search_term and "'" not in search_term:
         db_connection = MySQLdb.Connect(
             host, username, password, db_name, port)
         cursor = db_connection.cursor()
