c4df0864d30713753bce5f7ecb2f0adf817acc8e
mena5800 <mr.safwat55@gmail.com>
2023-11-16 01:38:28 +0200

    add 0x12

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const argv = process.argv;
+
+function add (a, b) {
+  return a + b;
+}
+
+const num1 = parseInt(argv[2]);
+const num2 = parseInt(argv[3]);
+
+console.log(add(num1, num2));
