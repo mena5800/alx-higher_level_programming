e4bfbb551d02028af27fefb82687c8f9ac2050f0
mena5800 <mr.safwat55@gmail.com>
2023-11-19 21:27:12 +0200

    solve from 7 to 11

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+"""
+script that prints the State object with the name passed as
+argument from the database hbtn_0e_6_usa.
+"""
+
+import sqlalchemy
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    engine = sqlalchemy.create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sqlalchemy.orm.sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(
+        State.name == sys.argv[4]).first()
+
+    if states:
+        print(states.id)
+    else:
+        print("Not found")
---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+"""
+this module i use Sqlalchemy to list all state objects from
+database hbtn_0e_6_usa
+"""
+
+import sqlalchemy
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    engine = sqlalchemy.create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sqlalchemy.orm.sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
