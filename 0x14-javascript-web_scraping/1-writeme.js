1212ee2a81231e70364caa3a2d77dd41f3e0f197
mena5800 <mr.safwat55@gmail.com>
2024-01-04 00:46:22 +0200

    solve 1 - 2 - 3 - 4 tasks

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const fs = require('fs');
+
+// Get the file path from command-line arguments
+const filePath = process.argv[2];
+const txtToWrite = process.argv[3];
+
+// Read the content of the file in utf-8
+fs.writeFile(filePath, txtToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+const WhatWeLook = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+request.get(url, (err, response) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const results = JSON.parse(response.body).results;
+    let counter = 0;
+    for (let i = 0; i < results.length; i++) {
+      for (let j = 0; j < results[i].characters.length; j++) {
+        if (results[i].characters[j] === WhatWeLook) {
+          counter += 1;
+        }
+      }
+    }
+    console.log(counter);
+  }
+});
