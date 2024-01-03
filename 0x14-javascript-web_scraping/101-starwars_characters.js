19b705f9937c26dba3844962072a60e864c38f27
mena5800 <mr.safwat55@gmail.com>
2024-01-04 01:55:56 +0200

    solve last task

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,28 @@
 #!/usr/bin/node
+const request = require('request');
+
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+function printCharacter (data, index) {
+  if (index < data.length) {
+    request.get(data[index], (err, response) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(JSON.parse(response.body).name);
+        printCharacter(data, index + 1);
+      }
+    });
+  }
+}
+
+request.get(url, async (err, response) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const data = JSON.parse(response.body).characters;
+    printCharacter(data, 0);
+  }
+}
+);
